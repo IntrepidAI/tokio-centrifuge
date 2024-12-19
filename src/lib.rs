686a671dd4ad01badd08a96bacf90d5b437a8ed9
Alex Kocharin <alex@kocharin.ru>
2024-12-19 17:03:48 +0400

    re-export tungstenite

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,7 @@ pub mod protocol;
 pub mod server;
 pub mod subscription;
 mod utils;
+
+// Server::serve requires tungstenite::Message, so we should
+// re-export it to make sure user has the same version.
+pub use async_tungstenite::tungstenite;
